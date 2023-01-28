2444e15b239e42d264373cf2f5c7afd92028bed0
Thibault P.V <Reus@reus186-1.home>
2023-01-28 06:09:20 +0100

    amelioration_darkmode

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ function changeDarkmode() {
 		darkmode = false;
 		document.documentElement.style.setProperty("--text-color", "black");
 		document.documentElement.style.setProperty("--background-image", "linear-gradient(to top, #fff1eb 0%, #ace0f9 100%)");
-		document.getElementById("dark-light-mode").innerHTML = "Dark mode";
+		document.getElementById("dark-light-mode").innerHTML = "[Dark mode]";
 	}
 	else {
 		darkmode = true;
 		document.documentElement.style.setProperty("--text-color", "#efe7e5");
 		document.documentElement.style.setProperty("--background-image", "linear-gradient(to right, #434343 0%, black 100%)");
-		document.getElementById("dark-light-mode").innerHTML = "Light mode";
+		document.getElementById("dark-light-mode").innerHTML = "[Light mode]";
 	}
 }
